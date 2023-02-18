699b6f80c6e2ba1084eeb2c13673ccaabcd26039
Chris Hendrix <chendrix1123@gmail.com>
2023-02-18 16:56:54 -0500

    Personalize data (#1)
    * resume data checkpoint
    
    * portfolio checkpoint
    
    * remove some sections
    
    * update readme

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -6,11 +6,9 @@ export const SkillGroup: FC<PropsWithChildren<{skillGroup: SkillGroupType}>> = m
   const {name, skills} = skillGroup;
   return (
     <div className="flex flex-col">
-      <span className="text-center text-lg font-bold">{name}</span>
+      <span className="text-left text-lg font-bold">{name}</span>
       <div className="flex flex-col gap-y-2">
-        {skills.map((skill, index) => (
-          <Skill key={`${skill.name}-${index}`} skill={skill} />
-        ))}
+        {skills}
       </div>
     </div>
   );
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-spacing */
 import {
   AcademicCapIcon,
   CalendarIcon,
@@ -9,22 +10,13 @@ import {
 } from '@heroicons/react/outline';
 
 import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
-import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
+import heroImage from '../images/header-background.jpg';
+import portfolioImage1 from '../images/portfolio/1-chat.jpg';
+import portfolioImage2 from '../images/portfolio/2-baseball.jpg';
+import portfolioImage3 from '../images/portfolio/3-nuclear.jpg';
+import portfolioImage4 from '../images/portfolio/4-earthquake.jpg';
+import portfolioImage5 from '../images/portfolio/5-mp3.jpg';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -44,8 +36,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Resume - Chris Hendrix',
+  description: "Chris Hendrix' software engineering resume",
 };
 
 /**
@@ -69,24 +61,24 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Chris Hendrix.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a New York based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
+        at <strong className="text-stone-100">Emissary.io</strong> helping connect people with game-changing account intelligence
+        from world-class leaders.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        <strong className="text-stone-100">Baseball</strong> is my lifelong passion, but I also enjoy&nbsp;
+        <strong className="text-stone-100">strength training</strong> and <strong className="text-stone-100">DJing</strong>&nbsp;
+        some tunes in my bedroom.
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
+      href: 'https://github.com/chris-hendrix/nextjs-resume/Resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: DownloadIcon,
@@ -104,16 +96,16 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `As a Structural/Seismic Engineering graduate from MIT, I always loved building things. In my first life, while providing seismic 
+  risk consulting in the nuclear power industry, I started gravitated toward more towards software-oriented projects, and eventually found my love 
+  for  building things on the web.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
+    { label: 'Location', text: 'New York, NY', Icon: MapIcon },
+    { label: 'Age', text: '33', Icon: CalendarIcon },
+    { label: 'Citizenship', text: 'United States', Icon: FlagIcon },
     {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    { label: 'Education', text: 'MIT', Icon: AcademicCapIcon },
+    { label: 'Employment', text: 'Emissary.io', Icon: OfficeBuildingIcon },
   ],
 };
 
@@ -122,72 +114,82 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'English',
-        level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
-    ],
+    name: 'Languages',
+    skills: 'Python, Javascript, Typescript, Java, SQL',
   },
   {
-    name: 'Frontend development',
-    skills: [
-      {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
-        level: 6,
-      },
-    ],
+    name: 'Technologies',
+    skills: 'Node, Express, flask, React, Postgres, MySQL, MongoDB, Redux',
   },
   {
-    name: 'Backend development',
-    skills: [
-      {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
-      },
-    ],
+    name: 'Tools',
+    skills: 'GCP, Docker, Github Actions, Elasticsearch, CI/CD, Linux, WSL2',
+  },
+];
+
+/**
+ * Resume section -- TODO: Standardize resume contact format or offer MDX
+ */
+
+export const experience: TimelineItem[] = [
+  {
+    date: 'Jan 2022 - Present',
+    location: 'Emissary.io',
+    title: 'Senior Fullstack Engineer',
+    content: `Emissary.io provides a platform for tech sellers to find game-changing account intelligence 
+      from world-class leaders. At Emissary.io, I`,
+    list: [
+      `Lead revenue-impacting features within the main app, including user search, library search, and email 
+      notifications by designing the new MySQL schema, flask endpoints, Elasticsearch indexing, sqlalchemy queries, 
+      pytest unit tests, and React components`,
+      `Planned feature epics by providing user stories based on PRDs as part of an agile development workflow, 
+      enabling the swift development of new features by the engineering team`,
+      `Lead the upgrade of MySQL in our local Docker environment, as well as our GCP production environment, 
+      resulting in improved query performance`,
+      `Tasked with mentoring and onboarding a new engineer shortly after I joined Emissary, enabling him to produce 
+      meaningful PRs within a month `
+    ]
   },
   {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
-      },
-    ],
+    date: 'July 2013 - Jan 2022',
+    location: 'Jensen Hughes (acquired Stevenson & Associates in 2017)',
+    title: 'Engineer',
+    content: `Jensen Hughes provides engineering consulting that makes our world safe, secure, and resilient. 
+      While at Jensen Hughes, I`,
+    list: [
+      `Managed a team of 4-6 engineers and technical experts, architected a relational database, and developed 
+      meaningful trends as a part of EPRI funded research on nuclear power risk assessments`,
+      `Generalized the existing Python/TensorFlow codebase so it could be used for different applications for 
+      a project designed to use NLP to streamline the processing of Nuclear Power Plant condition reports`,
+      `Designed the the object structure, database mapper for multiple desktop applications that are currently 
+      being used by nuclear power plants for seismic risk assessments`,
+      `Awarded by management for developing tools that automate proposal generation and track project budgets`,
+      `Performed non-software work including seismic risk assessments, blast design, proposals writing, project 
+      management, and leading internal BD initiatives`,
+    ]
+  },
+  {
+    date: 'Aug 2012 - May 2023',
+    location: 'MIT',
+    title: 'Teaching Assistant, Java',
+    content: `As a graduate student at MIT, I`,
+    list: [
+      `Taught weekly recitations going over lecture topics such as data structures, algorithms, and OOP`,
+      `Helped administer the course by writing test questions and homework solutions`,
+    ]
+  },
+];
+
+export const education: TimelineItem[] = [
+  {
+    date: '2012 - 2013',
+    location: 'MIT',
+    title: 'M.Eng., Seismic Engineering',
+  },
+  {
+    date: '2008 - 2012',
+    location: 'MIT',
+    title: 'B.S., Civil Engineering',
   },
 ];
 
@@ -196,113 +198,35 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage1,
+    title: 'Next.js Message App',
+    description: 'A "boilerplate" message app with full user authentication.',
+    url: 'https://boilerplate-nextjs-eight.vercel.app/',
+    image: portfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage2,
+    title: 'Fantasy Baseball App',
+    description: 'A custom fantasy baseball webapp built with React and Express.',
+    url: 'https://mit-fantasy-baseball.onrender.com/',
+    image: portfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage3,
+    title: 'AI Tool for Nuclear Power Plant Maintenance',
+    description: 'Tool that uses NLP to streamline nuclear power plant condition reporting.',
+    url: 'https://www.jensenhughes.com/digital-solutions/dataadvisr-ai-process-improvement',
+    image: portfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage4,
+    title: 'EPRI Seismic Research',
+    description: `Publication outlining a data based approach to performing nuclear seismic risk assessments. 
+    Image by macrovector on Freepik`,
+    url: 'https://www.epri.com/research/programs/061177/results/3002020572',
+    image: portfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
-    image: porfolioImage11,
-  },
-];
-
-/**
- * Resume section -- TODO: Standardize resume contact format or offer MDX
- */
-export const education: TimelineItem[] = [
-  {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-];
-
-export const experience: TimelineItem[] = [
-  {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
-  },
-  {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
-    content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
-    ),
+    title: 'MP3 Organizer',
+    description: `Simple .NET desktop app that can tag and organize your mp3 files in a datagrid view.`,
+    url: 'https://www.epri.com/research/programs/061177/results/3002020572',
+    image: portfolioImage5,
   },
 ];
 
@@ -317,16 +241,6 @@ export const testimonial: TestimonialSection = {
       text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
-    {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-    },
-    {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-    },
   ],
 };
 
@@ -340,23 +254,23 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'cmhendrix@alum.mit.edu',
+      href: 'mailto:cmhendrix@alum.mit.edu',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'New York, NY',
+      href: 'https://goo.gl/maps/LHKagmdDMgDJ7Zhh7',
     },
     {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      type: ContactType.LinkedIn,
+      text: '/in/chris-hendrix',
+      href: 'https://www.linkedin.com/in/chris-hendrix/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'chris-hendrix',
+      href: 'https://github.com/chris-hendrix',
     },
   ],
 };
@@ -365,9 +279,6 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/chris-hendrix' },
+  { label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/chris-hendrix/' },
 ];
